b1625524d9da0dada77e331f5aff0e192be3d60e
Sbeve <touilsaleheddine@com.r-iset.tn>
2026-01-01 15:04:24 +0100

    Remove header button container and associated functionality for cleaner layout

--- src/components/Header/index.jsx
+++ src/components/Header/index.jsx
@@ -69,11 +69,6 @@ export default function Index() {
                 </Magnetic>
             </div>
         </div>
-        <div ref={button} className={styles.headerButtonContainer}>
-            <Rounded onClick={() => {setIsActive(!isActive)}} className={`${styles.button}`}>
-                <div className={`${styles.burger} ${isActive ? styles.burgerActive : ""}`}></div>
-            </Rounded>
-        </div>
         <AnimatePresence mode="wait">
             {isActive && <Nav />}
         </AnimatePresence>
