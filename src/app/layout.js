6ba633402e94a1f249f9850a9cd627875c102e36
Sbeve <touilsaleheddine@com.r-iset.tn>
2026-05-20 13:46:20 +0100

    Remove Google Fonts import from globals.css, update layout.js to eliminate unused font variable, and adjust justify-content in Landing styles for improved layout consistency.

--- src/app/layout.js
+++ src/app/layout.js
@@ -1,8 +1,5 @@
 import './globals.css'
-import { Inter } from 'next/font/google'
 import Header from '../components/Header';
-
-const inter = Inter({ subsets: ['latin'] })
 
 export const metadata = {
   title: 'Saleh Eddine Touil | Bug Bounty Hunter & CPTS',
@@ -63,7 +60,7 @@ export const metadata = {
 export default function RootLayout({ children }) {
   return (
     <html lang="en">
-      <body className={inter.className}>
+      <body>
         <Header />
         {children}
       </body>
